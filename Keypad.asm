c97b50cb5daca870bce7b10a7fe83d6342ae21e7
Stefan Bossbaly <Sbossb@gmail.com>
2013-09-22 14:19:54 -0400

    Added the busy wait subroutine
    Was able to slow down the processor enought to see the different output
    LEDs light up. We need to implement a way so that we use a timer overflow
    instead of burning clock cycles.

--- Keypad.asm
+++ Keypad.asm
@@ -5,23 +5,29 @@
         LDX #$0000      ;Register base is 0x00
         BRA SETUP       ;GOTO main
         
-;ADD(int num1, int num2)
-;Adds two numbers together
-ADD:    CLRA            ;Clear A
-        CLRB            ;Clear B
+BWAIT:	LDD #$FFFF
+BLP:    CPD #$0000
+        BEQ BEND
+        SUBD #$0001
+        BRA BLP
+BEND:   RTS
+        
 SETUP:  LDAA #$0F
         STAA DDRB,x     ;Set 0x0F into DDRB this makes PB0-3 as output and 4-7 as inputs
-INPUT:  ;LDAA #$01       ;Set 0x01 into Port B
-        ;STAA PORTB,x    ;This sets PB0 as HIGH all other are LOW
-        ;LDAA #$02       ;Set 0x02 into Port B
-        ;STAA PORTB,x    ;This sets PB1 as HIGH all other are LOW
-	LDAA #$01       ;Set 0x04 into Port B
-	STAA PORTB,x    ;This sets PB3 as HIGH all other are LOW
-	;LDAA #$08       ;Set 0x08 into Port B
-        ;STAA PORTB,x    ;This sets PB4 as HIGH all other are LOW
-
-
-
-	;LDAA PORTB,x    ;Load Port B into A
+        CLRA            ;Clear A
+        CLRB            ;Clear B
+INPUT:  LDAA #$08       ;Set 0x08 into Port B
+        STAA PORTB,x    ;This sets PB4 as HIGH all other are LOW
+        JSR BWAIT
+        LDAA #$04       ;Set 0x04 into Port B
+        STAA PORTB,x    ;This sets PB3 as HIGH all other are LOW
+        JSR BWAIT
+        LDAA #$02       ;Set 0x02 into Port B
+        STAA PORTB,x    ;This sets PB1 as HIGH all other are LOW
+        JSR BWAIT
+        LDAA #$01       ;Set 0x01 into Port B
+        STAA PORTB,x    ;This sets PB0 as HIGH all other are LOW
+        SWI
+        ;LDAA PORTB,x    ;Load Port B into A
         ;ANDA #$80
         ;SWI
