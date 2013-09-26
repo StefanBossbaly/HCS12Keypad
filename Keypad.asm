506875d90abe84914f433b5655f156c06a8328c4
Stefan Bossbaly <Sbossb@gmail.com>
2013-09-26 09:32:15 -0400

    Added subroutine comments
    Added more comments to the subroutine to make them stand out

--- Keypad.asm
+++ Keypad.asm
@@ -3,13 +3,24 @@
 
         ORG $2000       ;Start at memory address 2000
         LDX #$0000      ;Register base is 0x00
-        CLI             ;Enable interrupts
-        BRA SETUP       ;GOTO main
+        BRA MAIN               ;GOTO main
         
-LETTER: DS.W 1
+        
+MAIN:   LDAA #$0F
+        STAA DDRB,x     ;Set 0x0F into DDRB this makes PB0-3 as output and 4-7 as inputs
+        CLRA            ;Clear A
+        CLRB            ;Clear B
+        LDAA #$70
+        STAA RTICTL,x
+        CLI             ;Enable interrupts
+LOOP:   BRA LOOP
 
+RTISR:  LDD #$1010
+        SWI
+;-------------------------------------------------------------------------------
 ;void BWAIT(void)
 ;Busy waits by counting down from 0xFFFF
+;-------------------------------------------------------------------------------
 BWAIT:  LDD #$FFFF      ;Load 0xFFFF into register D
 BLP:    CPD #$0000      ;Compare it with 0x0000
         BEQ BEND        ;End the loop if they are equal
@@ -17,10 +28,12 @@ BLP:    CPD #$0000      ;Compare it with 0x0000
         BRA BLP         ;Loop
 BEND:   RTS             ;Return to sub routine
 
+;-------------------------------------------------------------------------------
 ;int CINPUT(int column)
-;Sees if there is any input and returns it in the D register
+;Sees if there is any input on the column and returns it in the A register
 ;If there is no input then it returns 17
-CINPUT:	LDAA PORTB,x    ;Load port b into
+;-------------------------------------------------------------------------------
+CINPUT: LDAA PORTB,x    ;Load port b into
         ANDA #$F0       ;Get the upper nibble
         CMPA #$00       ;See if there is any input
         BEQ CNOIN       ;There is no input
@@ -48,12 +61,13 @@ CINR3:  LDAA #$0C
         RTS
 CNOIN:  LDAA #$11        ;Load 17 into A register
         RTS
-        
-SETUP:  LDAA #$0F
-        STAA DDRB,x     ;Set 0x0F into DDRB this makes PB0-3 as output and 4-7 as inputs
-        CLRA            ;Clear A
-        CLRB            ;Clear B
-INPUT0: LDAA #$08       ;Set 0x08 into Port B
+
+;-------------------------------------------------------------------------------
+;int KEYIO(void)
+;Sees if there is any input on keypad and returns it in the A register
+;If there is no input then it returns 17
+;-------------------------------------------------------------------------------
+KEYIO:  LDAA #$08       ;Set 0x08 into Port B
         STAA PORTB,x    ;This sets PB4 as HIGH all other are LOW
         JSR BWAIT `     ;Wait a bit for the input to prop
         LDAA #$00       ;Load 0 into A
@@ -62,11 +76,11 @@ INPUT0: LDAA #$08       ;Set 0x08 into Port B
         LEAS 1,SP       ;Put the stack back to normal
         CMPA #$11
         BNE HASIN
- 	LDAA #$04       ;Set 0x04 into Port B
+         LDAA #$04       ;Set 0x04 into Port B
         STAA PORTB,x    ;This sets PB3 as HIGH all other are LOW
         JSR BWAIT
-        LDAA #$01       ;Load 1 into A
-        PSHA            ;Push it onto the stack
+        LDAA #$01       ;Load 1 as column parameter
+        PSHA
         JSR CINPUT      ;Jump to the subroutine
         LEAS 1,SP       ;Put the stack back to normal
         CMPA #$11
@@ -89,7 +103,5 @@ INPUT0: LDAA #$08       ;Set 0x08 into Port B
         LEAS 1,SP       ;Put the stack back to normal
         CMPA #$11
         BNE HASIN
-HASIN:	SWI
-        ;LDAA PORTB,x    ;Load Port B into A
-        ;ANDA #$80
-        ;SWI
+        LDAA #$11
+HASIN:  RTS
